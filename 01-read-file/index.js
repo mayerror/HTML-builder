2c9706a78f1490dbdf1a3312d8eee04991fae7b7
Sergey Masiuk <mayerror@gmail.com>
2021-11-06 14:22:06 +0300

    feat: implement read file

---
+++ 01-read-file/index.js
@@ -0,0 +1,10 @@
+const fs = require('fs');
+const path = require('path');
+
+const filePath = path.join(__dirname, 'text.txt');
+
+let readableStream = fs.createReadStream(filePath, "utf8");
+ 
+readableStream.on("data", function(chunk){ 
+    console.log(chunk);
+});
